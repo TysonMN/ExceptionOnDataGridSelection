c670ce44bccaf835ed1eb8520597dce66f664f73
Tyson Williams <34664007+TysonMN@users.noreply.github.com>
2021-03-11 20:47:48 -0600

    Initial commit

---
+++ src/Project/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MyNamespace {
+  public class Command : ICommand {
+    private readonly Action _action;
+    public Command(Action action) => _action = action;
+    public event EventHandler CanExecuteChanged;
+    public bool CanExecute(object _) => true;
+    public void Execute(object _) => _action();
+  }
+
+  public partial class MainWindow : Window {
+    public ICommand MyCommand { get; }
+    public ObservableCollection<int> MyItems { get; } = new ObservableCollection<int>();
+    public int? MySelectedItem {
+      get => null;
+      set { }
+    }
+
+    public MainWindow() {
+      DataContext = this;
+      MyCommand = new Command(() => MyItems.Add(1));
+      InitializeComponent();
+    }
+  }
+}
